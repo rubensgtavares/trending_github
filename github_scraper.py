fa50989b670490e859d70192f9e0bde34766e0b1
Rubens Gabriel <135617827+rubensgtavares@users.noreply.github.com>
2025-03-31 20:39:41 -0300

    Update github_scraper.py

--- github_scraper.py
+++ github_scraper.py
@@ -15,6 +15,7 @@ def crawl_website(url: str, headers: dict) -> str | None:
     return None  # Retorna None se houver erro
 
 URL = 'https://github.com/trending'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 conteudo1 = crawl_website(url=URL,headers=headers)
 with open(file='10_trending_repo.html',mode='w', encoding='utf8') as fp:
     fp.write(conteudo1)
